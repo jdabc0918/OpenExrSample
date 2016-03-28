2d3ab2002bb2a024bcc532c67b22f1969719ff36
jdabc0918 <rival.mintjam@gmail.com>
2016-03-28 23:37:00 +0900

    update

--- ExrReaderSample/ExrReaderSample.cpp
+++ ExrReaderSample/ExrReaderSample.cpp
@@ -36,7 +36,7 @@ typedef struct HeaderInfo
 
 void readHeader(HeaderInfo  &info)
 {
-    FILE *m_fp = fopen("C:\\Users\\Yosuke\\Desktop\\test.exr", "rb");
+    FILE *m_fp = fopen("C:\\Users\\Yosuke\\Desktop\\untitled.exr", "rb");
 
     //  magic number
     fseek(m_fp, 0, SEEK_SET);
@@ -87,10 +87,100 @@ void readHeader(HeaderInfo  &info)
     info.height += 1;
 }
 
+int SearchText(unsigned char *data, int dataNum, std::string text)
+{
+    //  見つからなかったら-1を返す
+    if (dataNum < (int)text.length()) return -1;
+
+    int loc = -1;
+    for (int i = 0; i <= dataNum - (int)text.length(); i++)
+    {
+        std::string tmp;
+        for (int j = 0; j < (int)text.length(); j++)
+        {
+            tmp += (char)data[i + j];
+        }
+        if (tmp == text)
+        {
+            loc = i;
+            break;
+        }
+    }
+
+    return loc;
+}
+
+void readHeader2(HeaderInfo  &info)
+{
+    FILE *m_fp = fopen("C:\\Users\\Yosuke\\Desktop\\untitled2.exr", "rb");
+
+    //  一旦データをすべて読み込む
+    int dataByte = 0;
+    while (fgetc(m_fp) != EOF) dataByte++;   //  空読みしてバイト数を取得    
+    unsigned char *data = new unsigned char[dataByte];
+    fseek(m_fp, 0, SEEK_SET);
+    fread(data, sizeof(unsigned char), dataByte, m_fp);
+
+    //  ワーク変数
+    int ret, tmp;
+
+    //  magic number
+    info.magic_number = ((int*)data)[0];
+    //  version
+    info.version_number = ((int*)data)[1];
+    //  channnel
+    ret = SearchText(data, dataByte, "channels") + 16;
+    info.ch_data_size = *(int*)&data[ret];
+    ret += 4;
+    for (int i = 0; i < (info.ch_data_size - 1) / 18; i++)  //  channel name & type
+    {
+        //  index
+        tmp = ret + i * 18;
+        //  channel name
+        std::string ch_name_str((const char *)&data[tmp]);
+        info.ch_name.push_back(ch_name_str);
+        //  type
+        tmp += 2;
+        int type_index = *(int*)&data[tmp];
+        switch (type_index)
+        {
+        case 0:
+            info.ch_type.push_back("UINT");
+            break;
+        case 1:
+            info.ch_type.push_back("HALF");
+            break;
+        case 2:
+            info.ch_type.push_back("FLOAT");
+            break;
+        default:
+            info.ch_type.push_back("null");
+            break;
+        }
+    }
+    //  compression
+    ret = SearchText(data, dataByte, "compression") + 24;
+    if (*(int*)&data[ret] != 1)
+    {
+        std::cout << "無圧縮じゃないようです。。。" << std::endl;
+        return;
+    }
+    //  data window
+    ret = SearchText(data, dataByte, "dataWindow") + 29;
+    info.width = *(int*)&data[ret] + 1;
+    info.height = *(int*)&data[ret + 4] + 1;
+    //  start of scan line offset table
+    ret = SearchText(data, dataByte, "xDensity") + 24;  //ここいまいち。"xDensity"という文字列を見つけて+24
+    //  ★Start of Pixel Data
+    ret += 8 * info.height; //  オフセットテーブルを飛ばす
+
+    info.print();
+}
+
 int main(int argc, char **argv)
 {   
     HeaderInfo hi;
-    readHeader(hi);
+    readHeader2(hi);
 
     printf("hoge\n");
 
